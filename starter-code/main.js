f798c0fcafe2d176ce60ec2017db543dc07675e1
John Castrillon <Johnny@Johns-MBP.home>
2016-08-03 20:11:02 -0400

    added game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,16 @@
-console.log("JS file is connected to HTML! Woo!")
+/*console.log("JS file is connected to HTML! Woo!")
+get age
+
+if age >= 65
+    display message "Ticket price: $6.00"
+
+otherwise if age < 25
+    display message "Ticket price: $8.00"
+
+otherwise
+    display message "Ticket price: $10.00"*/
+
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "queen";
