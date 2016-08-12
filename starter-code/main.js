91b560b223dc0208b24e50387872484f54bcf249
John Castrillon <Johnny@Johns-MacBook-Pro.local>
2016-08-12 17:21:52 -0400

    Really trying to fix the card flip functions

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,15 +1,3 @@
-/*console.log("JS file is connected to HTML! Woo!")
-get age
-
-if age >= 65
-    display message "Ticket price: $6.00"
-
-otherwise if age < 25
-    display message "Ticket price: $8.00"
-
-otherwise
-    display message "Ticket price: $10.00"*/
-
 /*var cardOne = "king";
 var cardTwo = "queen";
 var cardThree = "king";
@@ -22,26 +10,36 @@ else {
   alert("Sorry, try again.")
 }*/
 
-var gameBoard = document.getElementById('game-board');
+// var gameBoard = document.getElementById('game-board');
 
-var cards = ["king", "queen", "king", "queen"];
-cards[0] = "king"
-cards[1] = "queen"
+// var cards = ["king", "queen", "king", "queen"];
+//
+// for(var i = 0; i < cards.length; i++) {
+//   var cardCreate = document.createElement('div');
+//   cardCreate.className += ' card';
+//
+// }
 
+// alternate strategy from talking to Raz
+// var cards = ["king", "queen", "king", "queen"]
+// cards[0] = "king"
+// cards[1] = "queen"
+// cards[2] = "king"
+// cards[3] = "queen"
 //user clicks on first card activates function cardFlip
 //console.log(this.style)
 //select random card
 //Math.random() //produces 0-0.99999999
 //Math.random() * 4 cards.length (starts off at 4)
-function filpCard() {
-  var randomIndex = Math.floor(Math.random()*cards.length) //(might be: 0, 1, 2, 3)
+// function flipCard() {
+  // var randomIndex = Math.floor(Math.random()*cards.length) //(might be: 0, 1, 2, 3)
   // => give me the card at index 3
-  var selectedCard = cards[randomIndex]; //selectedCard = "king" or "queen"
+  // var selectedCard = cards[randomIndex]; //selectedCard = "king" or "queen"
   // Now we want to get rid of it from this global array, cards
 
   //before: cards = ["king", "queen", "king", "queen"]; // cards.length = 3
 
-  cards.splice(randomIndex, 1)
+  // cards.splice(randomIndex, 1)
 
   /*First time clicked This happens...*/
   //after: cards = ["king", "queen", "queen"]; // cards.length = 3
@@ -55,36 +53,19 @@ function filpCard() {
   //if its the second click we check if its a match
   // How do you know if its the second click...?
   // One strategy is to check if cards.length is even or odd...
-    if(cards.length === 3 || cards.length === 1){
-      checkIfItIsAMatch();
-    }
-}
-function checkIfItIsAMatch(){
+    // if(cards.length === 3 || cards.length === 1){
+      // checkIfItIsAMatch();
+    // }
+// }
+// function checkIfItIsAMatch(){
   //here we check the background-image of each card is it showing the back or front
   //if showing front of both cards then is the background-image ==?
-  if()
-}
+  // if()
+// }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var cards = ['queen', 'queen', 'king', 'king'];
+var cards = ['queen', 'queen', 'king', 'king'];
 
 var cardsinPlay = [ ];
 
@@ -104,7 +85,7 @@ function createBoard() {
 function isTwoCards() {
 	cardsInPlay.push(this.getAttribute('data-card'));
 	console.log(this.getAttribute('data-card'));
-	if (this.getAttribute('data-card') == 'king') {
+	if (this.getAttribute('data-card') === 'king') {
 		this.innerHTML = '<img src="king card.jpg"/>';
 	}
 	else {
@@ -117,7 +98,7 @@ function isTwoCards() {
 }
 
 function isMatch(cards) {
-	if (card[0] === card[1]) {
+	if (cards[0] === cards[1]) {
 	alert("You found a match!");
 	}
 	else {
